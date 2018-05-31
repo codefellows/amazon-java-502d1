7614265a8e981a76ea9c58fa6ec9600cf33b1102
Steve Geluso <stevegeluso@gmail.com>
2018-05-31 14:31:40 -0700

    fixing NaN error

--- 16-k-nearest-neighbors/SeattleKNearestNeighborhoods/src/main/java/KNearestNeighborhoods.java
+++ 16-k-nearest-neighbors/SeattleKNearestNeighborhoods/src/main/java/KNearestNeighborhoods.java
@@ -11,18 +11,23 @@ import java.util.stream.Collectors;
 
 public class KNearestNeighborhoods {
     // https://www.google.com/maps/@47.6494201,-122.3322363,14z
-    public static MapPoint FREMONT_CENTER = MapPoint.fromStrings("Fremont", "47.6494201", "-122.3322363");
-    public static MapPoint WALLINGFORD_CENTER = MapPoint.fromStrings("Wallingford", "47.661966", "-122.330176");
-    public static MapPoint AMBIGUOUS_CENTER = MapPoint.fromStrings("Unknown", "47.657919","-122.342450");
+    public static MapPoint FREMONT_CENTER = new MapPoint("Fremont", 47.658178, -122.360320);
+    public static MapPoint WALLINGFORD_CENTER = new MapPoint("Wallingford", 47.661966, -122.330176);
+    public static MapPoint AMBIGUOUS_CENTER = new MapPoint("Unknown", 47.657919,-122.342450);
 
     public static void main(String[] args) {
         try {
-            String csvPath = "/Users/moonmayor/Fellows/amazon-java-501d1/18-k-nearest-neighbors/SeattleKNearestNeighborhoods/src/main/resources/listings.csv";
+            String csvPath = "/Users/moonmayor/Fellows/amazon-java-501d1/16-k-nearest-neighbors/SeattleKNearestNeighborhoods/src/main/resources/listings.csv";
             CSVReader reader = new CSVReader(new FileReader(csvPath));
             List<MapPoint> points = reader.readAll().stream()
             .skip(1)
+            .map(cols -> {
+                System.out.println(cols);
+                return cols;
+            })
             .map(columns -> {
-                String neighborhood = columns[10];
+                //String neighborhood = columns[10];
+                String neighborhood = columns[36];
                 String latitude = columns[45];
                 String longitude = columns[46];
 
@@ -32,8 +37,13 @@ public class KNearestNeighborhoods {
                 System.out.println();
                 return MapPoint.fromStrings(neighborhood, latitude, longitude);
             })
+            .filter(bnb -> bnb != null)
             .filter(bnb -> bnb.isFremont() || bnb.isWallingford())
             .collect(Collectors.toList());
+
+            System.out.println("points: " + points);
+            System.out.println("Total points: " + points.size());
+            System.out.println();
 
             String label1 = kNearestNeighbords(3, points, FREMONT_CENTER);
             System.out.println(label1);
--- 16-k-nearest-neighbors/SeattleKNearestNeighborhoods/src/main/java/MapPoint.java
+++ 16-k-nearest-neighbors/SeattleKNearestNeighborhoods/src/main/java/MapPoint.java
@@ -6,6 +6,9 @@ public class MapPoint {
     public static MapPoint fromStrings(String description, String latt, String longg) {
         double latitude = Double.parseDouble(latt);
         double longitude = Double.parseDouble(longg);
+        if (Double.isNaN(latitude) || Double.isNaN(longitude)) {
+            return null;
+        }
         return new MapPoint(description, latitude, longitude);
     }
 
@@ -37,7 +40,11 @@ public class MapPoint {
     public Distance distance(MapPoint other) {
         double deltaLat = other.latitude - this.latitude;
         double deltaLong = other.longitude - this.longitude;
-        double distance = Math.sqrt(deltaLat * deltaLat + deltaLong + deltaLong);
+        double distance = Math.sqrt(deltaLat * deltaLat + deltaLong * deltaLong);
         return new Distance(distance, this);
+    }
+
+    public String toString() {
+        return this.neighborhood();
     }
 }
